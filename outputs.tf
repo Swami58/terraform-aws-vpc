96e49a2ba640696531a427b54cfd17f475fc6dba
Swami58 <n151302@rguktn.ac.in>
2025-04-17 12:13:16 +0530

    aws vpc

--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,29 @@
 #   value = data.aws_availability_zones.available.names
 #}
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = aws_vpc.main.id
 }
 
-output "public_subnet_list" {
-  value = module.vpc.public_subnet_ids
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
+
+output "database_subnet_group_id" {
+  value = aws_db_subnet_group.default.id
+}
+
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
 }
 
 output "igw_id" {
-  value = module.vpc.igw_id
+  value = aws_internet_gateway.gw.id
 }
